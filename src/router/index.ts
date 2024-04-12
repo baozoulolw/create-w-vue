b32d54dd5adf22d9b9dd426b79fa03c9db66477d
Baozoulolw <Baozoulolw@gmail.com>
2024-04-12 21:44:27 +0800

    fix: 优化文件结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,34 +3,13 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/home'
   },
   {
     //路由初始指向
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/login/login.vue')
-  },
-  {
-    path: '/home',
-    redirect: '/home/message'
-  },
-  {
     path: '/home',
     name: 'home',
-    component: () => import('../views/home/home.vue'),
-    children: [
-      {
-        path:'/home/message',
-        name:'message',
-        component: () => import('../views/home/message/message.vue'),
-      },
-      {
-        path:'/home/personal',
-        name:'personal',
-        component: () => import('../views/home/personal/personal.vue'),
-      }
-    ]
+    component: () => import('../views/index.vue')
   }
 ]
 
---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>create-w-vue</div>
+</template>
+
+<script setup>
+import { reactive, onMounted } from 'vue'
+const data = reactive({})
+onMounted(() => {
+})
+</script>
+<style scoped lang='scss'>
+</style>
